486dd8da7785d39768cd2b097a891dd81a435713
Nathaniel Vala <nathanielvala@hotmail.com>
2021-09-14 21:03:21 +1000

    version 0.1 working

--- tabSaverConsoleDump.js
+++ tabSaverConsoleDump.js
@@ -1,8 +1,11 @@
-chrome.windows.getAll({populate:true},function(windows){
-    windows.forEach(function(window){
-      window.tabs.forEach(function(tab){
-        //collect all of the urls here, I will just log them instead
-        console.log(tab.url);
-      });
+document.getElementById("dumpbutton").addEventListener("click", function() {
+    console.log("entering function");
+    chrome.windows.getAll({ populate: true }, function(windows) {
+        windows.forEach(function(window) {
+            window.tabs.forEach(function(tab) {
+                //collect all of the urls here, I will just log them instead
+                console.log("Window:" + window.id + ",TabURL:" + tab.url);
+            });
+        });
     });
 });
